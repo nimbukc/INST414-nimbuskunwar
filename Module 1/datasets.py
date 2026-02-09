614a9e5b4b780e56bd598ab8d4f4321cc2183556
Nimbus Kunwar <nimbukunwar@gmail.com>
2026-02-09 00:56:03 -0500

    Delete Module 1 directory

--- Module 1/datasets.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-# Load New York Times COVID deaths data
-deaths_df = pd.read_csv("us-states.csv")
-
-# Optional: load vaccination data
-vax_file = "COVID-19_Vaccinations_in_the_United_States,Jurisdiction.csv"
